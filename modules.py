f6c12928b94a7914e5b4b09905e5deeb0d3fe9d5
julius6868 <32232115+julius6868@users.noreply.github.com>
2022-04-21 16:20:04 +1000

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,151 @@
+
+import torch
+from torch.nn import functional as f
+import torch.nn as nn
+from models.func.utils import mycorr
+
+
+class Attention(nn.Module):
+    def __init__(self, channel, reduction=2):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d((None, 1))
+        self.fc = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False),
+        )
+        self.nolinear = nn.Sigmoid()
+
+    def forward(self, x):
+        B, M, N, _ = x.size()
+        adj = mycorr(x)
+        x_ = self._prepare_attentional_mechanism_input(x)
+        e = self.avg_pool(x_)
+        e = e.permute(0, 2, 1, 3)
+        e = self.fc(e)
+        e = e.permute(0, 2, 1, 3)
+        e = e.view(B, M, N, N)
+        e = self.nolinear(e)
+        zero_vec_adj = -1e12 * torch.ones_like(adj)
+        attention = torch.where(adj > 0, e, zero_vec_adj)
+        attention = f.softmax(attention, dim=-1)
+        x = torch.matmul(attention, x)
+        return x
+
+    def _prepare_attentional_mechanism_input(self, Wh):
+        B, M, N, D = Wh.size()  # number of nodes
+        Wh_repeated_in_chunks = Wh.repeat_interleave(N, dim=-2)  # 对每一个特征向量重复N次
+        Wh_repeated_alternating = Wh.repeat(1, 1, N, 1)  # 将特征矩阵重复N次
+        pos = 2 * torch.ones_like(Wh_repeated_alternating)
+        one_vec = torch.ones_like(Wh_repeated_alternating)
+        pos = torch.where(Wh_repeated_in_chunks - Wh_repeated_alternating == 0, one_vec, pos)
+        Wh_repeated_alternating = pos * Wh_repeated_alternating
+        all_combinations_matrix = torch.cat([Wh_repeated_in_chunks, Wh_repeated_alternating], dim=-1)
+        # all_combinations_matrix = all_combinations_matrix.permute(0, 2, 1, 3)
+        return all_combinations_matrix
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, kernel_size, stride):
+        super().__init__()
+        """
+        in_channels: segments
+        mid_channels: last multi-head size
+        out_channels: new multi-head size
+        """
+        self.attn = Attention(in_channels*in_channels)
+        padding = kernel_size//2
+        self.mid_channels_ = int((mid_channels - kernel_size + 2 * padding) // stride + 1)
+
+        self.layer_1 = nn.Sequential(
+            nn.Conv2d(mid_channels, mid_channels, 1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            self.attn,
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+        )
+
+        self.layer_2 = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                      groups=in_channels, bias=False),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True)
+
+        )
+
+        self.layer_3 = nn.Conv2d(self.mid_channels_, out_channels, 1, bias=False)
+
+        if stride == 1:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(mid_channels, out_channels, 1, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(mid_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        self.stride = stride
+
+    def forward(self, x):
+        """
+        :input x: Batch, node, multi-head, dim
+        :return: Batch, multi-head, node, dim
+        """
+
+        """
+        shortcut
+        """
+        res = x.permute(0, 2, 1, 3)
+        shortcut = self.shortcut(res)
+        if self.stride != 1:
+            shortcut = torch.sum(shortcut, dim=-2).unsqueeze(-2)
+
+        """
+        Globel Node Attention
+        """
+        x = x.permute(0, 2, 1, 3)
+        out = self.layer_1(x)
+        out = out.permute(0, 2, 1, 3)
+
+        """
+        extract features from every node channels by using group
+        Batch, node, multi-head, dim
+        """
+        out = self.layer_2(out)
+        """
+        Batch, multi-head, node, dim
+        """
+        out = out.permute(0, 2, 1, 3)
+        """
+        increase the heads
+        # """
+        out = self.layer_3(out)
+        out += shortcut
+        out = out.permute(0, 2, 1, 3)
+        return out
+
+
+
+if __name__ == "__main__":
+    """
+    class Block(nn.Module) 测试
+    """
+    data = torch.randn(128, 9, 32, 200)
+    model_2 = Block(9, 32, 64, 7, 2)
+    out_1 = model_2(data)
+    print("out_1 size", out_1.size())
+
+    # model = Unfold((2, 150), 2)
+    # result = model(out_1)
+    # print(result.size())
+    # model_3 = Block(5, 64, 64, 5, 1)
+    # out_2 = model_3(result)
+    # print("out_2 size", out_2.size())
+    # print()
+    # model_ = Unfold((2, 30), 1)
+    # data_ = model_(result)
+    # print("new input:", data_.size())
+    # model2 = Block(18, 64, 128, 5, 5, 2)
+    # r = model2(data_)
+    # print(r.size())
---
+++ node_generator.py
@@ -0,0 +1,204 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as f
+from models.func.utils import myvif
+
+
+class Unfold(nn.Module):
+    """
+    调节stride的大小可以实现overlapping的功能
+    input size: B, 1, 1, D (2D 卷积， 需要四维input)
+    output size: B, N, D
+    permute and unsqueeze: B, N, 1, D
+    """
+    def __init__(self, kernel_size, stride):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def forward(self, x):
+        # a = x.permute(0, 2, 1, 3)
+        b = f.unfold(x, kernel_size=self.kernel_size, stride=self.stride)
+        b = b.permute(0, 2, 1)
+        b = b.unsqueeze(-2)
+        return b
+
+
+class ResLayer(nn.Module):
+    def __init__(self, channel, reduction=2):
+        super(ResLayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1)
+        return x * y.expand_as(x)
+
+
+class ResBasicBlock(nn.Module):
+    "Residual Squeeze-and-Excitation(SE) Block"
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, reduction=4):
+        super(ResBasicBlock, self).__init__()
+        self.conv1 = nn.Conv1d(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm1d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv1d(planes, planes, 1)
+        self.bn2 = nn.BatchNorm1d(planes)
+        self.reslayer = ResLayer(planes, reduction)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.reslayer(out)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class Node2Vec(nn.Module):
+    def __init__(self, afr_reduced_cnn_size, dropout):
+        super(Node2Vec, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=49, stride=6, bias=False, padding=int(49//2)),
+            nn.BatchNorm1d(64),
+            nn.GELU(),
+            nn.MaxPool1d(kernel_size=7, stride=4, padding=int(7//2)),
+            nn.Dropout(dropout),
+
+            nn.Conv1d(64, 128, kernel_size=7, stride=1, bias=False, padding=int(7//2)),
+            nn.BatchNorm1d(128),
+            nn.GELU(),
+
+            nn.Conv1d(128, 128, kernel_size=7, stride=1, bias=False, padding=int(7//2)),
+            nn.BatchNorm1d(128),
+            nn.GELU(),
+
+            nn.MaxPool1d(kernel_size=3, stride=4, padding=int(3//2)),
+            nn.Dropout(dropout)
+        )
+
+        self.inplanes = 128
+        self.AFR = self._make_layer(ResBasicBlock, afr_reduced_cnn_size, 1)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv1d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm1d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.AFR(x)
+        return x
+
+
+class GNA(nn.Module):
+    def __init__(self, channel, reduction=2):
+        super(GNA, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x)
+        y = self.fc(y)
+        return x * y.expand_as(x)
+
+
+class NodeGenerator(nn.Module):
+    def __init__(self, segment_size, overlapping_rate, in_channels, dropout=0.5):
+        super(NodeGenerator, self).__init__()
+        """
+        1. 输入： 需要是一个4D的数据
+        2. in_channels是选定segment大小，使用滑动窗口+overlapping后的segments数量，需要计算
+        3. 输出： Batch, segments, multi-head, dim
+        """
+        self.overlapping = int(segment_size * overlapping_rate)
+        "滑动窗口，截取segments"
+        self.unflod = Unfold((1, segment_size), self.overlapping)
+        self.node2vec = Node2Vec(30, dropout)
+        self.gna = nn.Sequential(
+            GNA(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, in_channels, 1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_channels),
+        )
+
+    def forward(self, x):
+        node_meta = []
+        x = self.unflod(x)
+        x = x.squeeze()
+        node_size = x.size()[1]
+        for idx in range(node_size):
+            data = x[:, idx, :]
+            data = data.unsqueeze(1)
+            out = self.node2vec(data)
+            out = out.view(x.size()[0], 1, -1)
+            node_meta.append(out)
+        node = torch.cat(node_meta, dim=1)
+        node = node.unsqueeze(2)
+        vif = myvif(node)
+        vif = vif.squeeze()
+        node = self.gna(node)
+        return vif, node
+
+
+class MultiHead(nn.Module):
+    def __init__(self, out_channels):
+        super(MultiHead, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(1, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1, 3)
+        "multi-head"
+        x = self.layer(x)
+        x = x.permute(0, 2, 1, 3)
+        return x
+
+
+if __name__ == "__main__":
+    """
+    class PrepBlock(nn.Module) 测试
+    """
+    raw_data = torch.randn(128, 1, 1, 3000)
+    model_1 = NodeGenerator(600, 0.5, 9)
+    model_2 = MultiHead(32)
+    vif, pred_data = model_1(raw_data)
+    print("pred data size:", pred_data.size())
+    print(vif.size())
+    out = model_2(pred_data)
+    print(out.size())
